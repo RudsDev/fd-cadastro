0c502f3ef47e33a1ecb050861aac7869840d95a6
Rafael Ulisses <ruds.devel@gmail.com>
2024-02-20 19:28:02 -0300

    Criação de modelo Usuario

---
+++ src/main/java/com/firstdecision/cadatro/api/domain/models/Usuario.java
@@ -0,0 +1,56 @@
+package com.firstdecision.cadatro.api.domain.models;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class Usuario {
+    
+    @NotBlank
+    @Size(min = 3, max = 50)
+    private String nome;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 6, max = 20)
+    private String senha;
+    private String confirmacaoSenha;
+    
+    public Usuario(String senha, String confirmacaoSenha) {
+        this.senha = senha;
+        this.confirmacaoSenha = confirmacaoSenha;
+    }
+
+    public Usuario(String nome, String senha, String confirmacaoSenha) {
+        this.nome = nome;
+        this.senha = senha;
+        this.confirmacaoSenha = confirmacaoSenha;
+    }
+
+    public Usuario(String nome, String email, String senha, String confirmacaoSenha) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.confirmacaoSenha = confirmacaoSenha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public String getConfirmacaoSenha() {
+        return confirmacaoSenha;
+    }
+
+    public boolean isSenhasIguais() {
+        return senha.equals(confirmacaoSenha);
+    }
+   
+}
---
+++ src/test/java/com/firstdecision/cadatro/api/domain/model/UsuarioTest.java
@@ -0,0 +1,168 @@
+package com.firstdecision.cadatro.api.domain.model;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.firstdecision.cadatro.api.domain.models.Usuario;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+
+public class UsuarioTest {
+
+    private Validator validator;
+
+    final String senha = "123456";
+    final String confirmacaoSenha = "654321";
+    final String email = "pedro@mail";
+    final String nome = "Pedro de Alcântara";
+    final String nomeGrande = "Pedro de Alcântara João Carlos Leopoldo Salvador Bibiano Francisco Xavier de Paula Leocádio Miguel Gabriel Rafael Gonzaga";
+    
+
+    @BeforeEach
+    void setUp() {
+        var factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    @Test
+    void deveRetonarFalse_QuandoSenhasDiferentes() {
+        var usuario = new Usuario(senha, confirmacaoSenha);
+        boolean actual = usuario.isSenhasIguais();
+        assertFalse(actual);
+    }
+
+    @Test
+    void deveRetonarTrue_QuandoSenhasIguais() {
+        String confirmacaoSenha = senha;
+        var usuario = new Usuario(senha, confirmacaoSenha);
+        boolean actual = usuario.isSenhasIguais();
+        assertTrue(actual);
+    }
+
+    @Test
+    void deveConterValidacoes_QuandoSenhaStringVazia() {
+        String senha = "";
+        var usuario = new Usuario(nome, email, senha, confirmacaoSenha);
+        Set<ConstraintViolation<Usuario>> violations = validator.validate(usuario);
+        assertFalse(violations.isEmpty());
+    }
+
+    @Test
+    void deveConterValidacoes_QuandoSenhaEspacoEmBranco() {
+        String senha = " ";
+        var usuario = new Usuario(nome, email, senha, confirmacaoSenha);
+        Set<ConstraintViolation<Usuario>> violations = validator.validate(usuario);
+        assertFalse(violations.isEmpty());
+    }
+
+    @Test
+    void deveConterValidacoes_QuandoSenhaStringNull() {
+        String senha = null;
+        var usuario = new Usuario(nome, email, senha, confirmacaoSenha);
+        Set<ConstraintViolation<Usuario>> violations = validator.validate(usuario);
+        assertFalse(violations.isEmpty());
+    }
+
+    @Test
+    void deveConterValidacoes_QuandoSenhaStringMenorQueSeis() {
+        String senha = "123";
+        var usuario = new Usuario(nome, email, senha, confirmacaoSenha);
+        Set<ConstraintViolation<Usuario>> violations = validator.validate(usuario);
+        assertFalse(violations.isEmpty());
+    }
+
+    @Test
+    void deveConterValidacoes_QuandoSenhaStringMaiorQueVinte() {
+        String senha = "Uma senha que é maior que vinte caracteres";
+        var usuario = new Usuario(nome, email, senha, confirmacaoSenha);
+        Set<ConstraintViolation<Usuario>> violations = validator.validate(usuario);
+        assertFalse(violations.isEmpty());
+    }
+
+    @Test
+    void deveConterValidacoes_QuandoNomeStringVazia() {
+        String nome = "";
+        var usuario = new Usuario(nome, email, senha, confirmacaoSenha);
+        Set<ConstraintViolation<Usuario>> violations = validator.validate(usuario);
+        assertFalse(violations.isEmpty());
+    }
+
+    @Test
+    void deveConterValidacoes_QuandoNomeStringEspacoEmBranco() {
+        String nome = " ";
+        var usuario = new Usuario(nome, email, senha, confirmacaoSenha);
+        Set<ConstraintViolation<Usuario>> violations = validator.validate(usuario);
+        assertFalse(violations.isEmpty());
+    }
+
+    @Test
+    void deveConterValidacoes_QuandoNomeStringNull() {
+        String nome = " ";
+        var usuario = new Usuario(nome, email, senha, confirmacaoSenha);
+        Set<ConstraintViolation<Usuario>> violations = validator.validate(usuario);
+        assertFalse(violations.isEmpty());
+    }
+
+    @Test
+    void deveConterValidacoes_QuandoNomeStringMenorQueTres() {
+        String nome = "Zé";
+        var usuario = new Usuario(nome, email, senha, confirmacaoSenha);
+        Set<ConstraintViolation<Usuario>> violations = validator.validate(usuario);
+        assertFalse(violations.isEmpty());
+    }
+
+    @Test
+    void deveConterValidacoes_QuandoNomeStringMaiorQueCinquenta() {
+        String nome = "Pedro de Alcântara João Carlos Leopoldo Salvador Bibiano Francisco Xavier de Paula Leocádio Miguel Gabriel Rafael Gonzaga";
+        var usuario = new Usuario(nome, email, senha, confirmacaoSenha);
+        Set<ConstraintViolation<Usuario>> violations = validator.validate(usuario);
+        assertFalse(violations.isEmpty());
+    }
+
+    @Test
+    void deveConterValidacoes_QuandoEmailStringVazia() {
+        String nome = "Pedro de Alcântara";
+        String email = "";
+        var usuario = new Usuario(nome, email, senha, confirmacaoSenha);
+        Set<ConstraintViolation<Usuario>> violations = validator.validate(usuario);
+        assertFalse(violations.isEmpty());
+    }
+
+    @Test
+    void deveConterValidacoes_QuandoEmailEspacoEmBranco() {
+        String email = " ";
+        var usuario = new Usuario(nome, email, senha, confirmacaoSenha);
+        Set<ConstraintViolation<Usuario>> violations = validator.validate(usuario);
+        assertFalse(violations.isEmpty());
+    }
+
+    @Test
+    void deveConterValidacoes_QuandoEmailStringNull() {
+        String email = null;
+        var usuario = new Usuario(nome, email, senha, confirmacaoSenha);
+        Set<ConstraintViolation<Usuario>> violations = validator.validate(usuario);
+        assertFalse(violations.isEmpty());
+    }
+
+    @Test
+    void deveConterValidacoes_QuandoEmailInvalido() {
+        String email = "p@dro@mail";
+        var usuario = new Usuario(nome, email, senha, confirmacaoSenha);
+        Set<ConstraintViolation<Usuario>> violations = validator.validate(usuario);
+        assertFalse(violations.isEmpty());
+    }
+
+    @Test
+    void deveNaoConterValidacoes_QuandoEmailValido() {
+        var usuario = new Usuario(nome, email, senha, confirmacaoSenha);
+        Set<ConstraintViolation<Usuario>> violations = validator.validate(usuario);
+        assertTrue(violations.isEmpty());
+    }
+}
