dbd97addfad93fd81ec582a40ccad13991636ece
Rafael Ulisses <ruds.devel@gmail.com>
2024-02-21 21:59:26 -0300

    Persistindo cadastro

--- src/main/java/com/firstdecision/cadatro/api/api/services/UsuarioService.java
+++ src/main/java/com/firstdecision/cadatro/api/api/services/UsuarioService.java
@@ -1,14 +1,22 @@
 package com.firstdecision.cadatro.api.api.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.firstdecision.cadatro.api.api.repository.UsuarioRepository;
 import com.firstdecision.cadatro.api.domain.exceptions.NegocioException;
 import com.firstdecision.cadatro.api.domain.models.Usuario;
 
+@Service
 public class UsuarioService {
+    
+    @Autowired
+    UsuarioRepository repository;
     
     public Usuario salvar(Usuario usuario) throws NegocioException {
         if (!usuario.isSenhasIguais())
             throw new NegocioException("Senha e confirmação de senha devem ser iguais!");
-        return usuario;
+        return repository.save(usuario);
     }
 
 }
